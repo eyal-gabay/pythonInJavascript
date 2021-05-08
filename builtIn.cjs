82b1968e8e3a14e8c4d4cd61d342355f3e7dd56e
eyal-gabay <62162460+eyal-gabay@users.noreply.github.com>
2021-05-08 18:00:00 +0000

    Update builtIn.cjs

--- builtIn.cjs
+++ builtIn.cjs
@@ -4,7 +4,16 @@ import_file = (library) => global.library = require(library)
 print = console.log
 str = (string) => string.toString()
 len = (object) => object.length
-__file__ = () => __filename
+hex = (number) => "0x" + number.toString(16)
+ord = (string) => string.charCodeAt(0)
+exit = (code) => process.exit(code)
+bool = (obj) => {
+    if (0 === obj.length){return false}
+    else if (Object.keys(obj).length === 0 && typeof obj === 'object'){return false}
+    else return Boolean(obj)
+}
+
+// __file__ = __filename
 
 function open(file, mode){
     import_file("fs")
@@ -17,6 +26,5 @@ function open(file, mode){
 if (require.main === module){
     print(open("file.txt", "r"))
 }
-
 
 
