1493d7a81b368786e9d89096368ed751876a5368
eyal-gabay <62162460+eyal-gabay@users.noreply.github.com>
2021-05-15 01:21:59 +0000

    Update builtIn.cjs

--- builtIn.cjs
+++ builtIn.cjs
@@ -7,13 +7,20 @@ len = (object) => object.length
 hex = (number) => "0x" + number.toString(16)
 ord = (string) => string.charCodeAt(0)
 exit = (code) => process.exit(code)
+quit = exit
+type = (obj) => typeof obj
 bool = (obj) => {
     if (0 === obj.length){return false}
     else if (Object.keys(obj).length === 0 && typeof obj === 'object'){return false}
     else return Boolean(obj)
 }
 
-// __file__ = __filename
+any = (list) => {
+    for (i = 0; i < len(list); i++){
+        print(bool(i))
+    }
+}
+
 sum = (iter, type) => {
     if (Array.isArray(iter)){
         return iter.reduce((a, b) => a + b, 0)
@@ -21,15 +28,18 @@ sum = (iter, type) => {
 }
 
 function open(file, mode){
-    import_file("fs")
+    // import_file("fs")
+    let read
     if (mode === "r"){
-        fs.readFile(file, "utf8", (error, data) => {return data})
+        // fs.readFile(file, "utf8", (error, data) => {global(read) = data; print(data)})
+        // print()
+        return read
     }
 }
 
 
 if (require.main === module){
-    print(open("file.txt", "r"))
+    // print(open("file.txt", "r"))
+    any([1, []])
 }
-
 
